f15b33cb71cd3e4dc4721b5b3e3bc02047e1ccc9
TwixGamer00 <64733980+TwixGamer00@users.noreply.github.com>
2021-12-03 14:30:01 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+cd /var/www/pterodactyl
+php artisan down
+git clone https://github.com/TwixGamer00/darknred.git
+rm -r resources
+rm -r public
+rm -r tailwind.config.js
+mv darknred/resources resources
+mv darknred/public public
+mv darknred/tailwind.config.js tailwind.config.js
+chmod -R 755 storage/* bootstrap/cache/
+npm i -g n
+yarn build
+php artisan queue:restart
+php artisan up
